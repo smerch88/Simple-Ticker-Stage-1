ea06cf6bfcde517820796694b8fb379e545ddd05
smerch88 <maxarmyk@gmail.com>
2022-08-03 13:17:26 +0300

    modified writing data to track.json

--- Python_Code/taskmanager/main/views.py
+++ Python_Code/taskmanager/main/views.py
@@ -176,8 +176,8 @@ def track(request):
 
 def index(request):
     try:
-        with open('track.json', 'w') as file:
-            file.write(str(request.headers))
-        return HttpResponse('Запрос сохранён в файл track.json')
+        with open('track.json', 'a') as file:
+            file.write(str(request.headers) + '\n')
+        return HttpResponse('Request saved to track.json')
     except:
-        return HttpResponse('Ошибка при работе с файлом')
+        return HttpResponse('Error!')
