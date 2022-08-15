01bf70e1a512c743bb1851b188b85c36edbfa9ab
Alexander Osnova <kephik2000@gmail.com>
2022-08-15 23:31:03 +0300

    update template of App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,11 @@
 import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import ApiService from './ApiService';
 import './App.css';
 import './App.scss';
@@ -14,9 +21,59 @@ export default function App() {
 
 
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+      A <Switch> looks through all its children <Route>
+      elements and renders the first one whose path
+      matches the current URL. Use a <Switch> any time
+      you have multiple routes, but you want only one
+      of them to render at a time
+    */}
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
     </div>
   );
 }
