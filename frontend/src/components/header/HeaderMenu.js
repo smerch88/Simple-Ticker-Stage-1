48e3e03fb85518cbe0562bb7e43365a544070c0c
Ruslan Russel <selezen.ua30@gmail.com>
2022-08-17 22:04:46 +0300

    Эффекты header
    Добавлены эффекты к ссылкам в header

--- frontend/src/components/header/HeaderMenu.js
+++ frontend/src/components/header/HeaderMenu.js
@@ -15,7 +15,7 @@ const HeaderMenu = () => {
                     <nav className="nav">
                         <ul className="nav__list">
                             <li className="nav__item"><a href="#" className="nav__link">About the product</a></li>
-                            <li className="nav__item"><a href="#" className="nav__link">Catalog</a></li>
+                            <li className="nav__item"><a href="" className="nav__link">Catalog</a></li>
                             <li className="nav__item"><a href="#" className="nav__link">About us</a></li>
                             <li className="nav__item"><a href="#" className="nav__link">News</a></li>
                         </ul>
