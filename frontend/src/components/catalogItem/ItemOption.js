94ad35ed162787e265600bc4147c8c8adc4d3323
Ruslan Russel <selezen.ua30@gmail.com>
2022-09-03 22:04:06 +0300

    Динамическое создание карточек товара

--- frontend/src/components/catalogItem/ItemOption.js
+++ frontend/src/components/catalogItem/ItemOption.js
@@ -1,44 +1,122 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import useProductService from "../../services/ProductService";
 
-const ItemOption = () => {
+const ItemOption = (props) => {
 
+    const [optionList, setOptionList] = useState([]) 
     const [showProp, setShowProp] = useState(false)
 
-    const showProperty = () => {
-        setShowProp(showProp => !showProp)
+    const myRefs = useRef([])
+
+    const {getAllProducts} = useProductService()
+
+   
+    const showProperty = (id) => {
+        
+        myRefs.current.forEach((item, i) => {
+            if (id !== i) {
+                item.classList.remove("active")
+            }
+        })
+        myRefs.current[id].classList.toggle("active")
+
     }
 
-    return (
-        <div className="ticker__option ">
-            <div 
-            className={showProp ? "ticker__config ticker__config_active" : "ticker__config"}>
-                <span className="ticker__name-property">
-                    Configuration:  
-                </span>
-                <span className="ticker__property">
-                    <ul>
-                        <li className='change'>Blue</li>
-                        <li 
-                        className={showProp ? "list-property active" : "list-property"}>
-                            <ul>
-                                <li className="active">Blue</li>
-                                <li>White</li>
-                                <li>Gold</li>
-                                <li>Red</li>
-                                <li>Black</li>
-                                <li>Purple</li>
-                                <li>Green</li>  
-                            </ul>
-                            <div className="ticker__property-change"></div>
-                        </li>
+
+    
+
+    const Request = () => {
+        getAllProducts()
+        .then(onOptionList)
+    }
+    
+    const onOptionList = (option) => {
+        setOptionList(option)
+        console.log(optionList)
+    }
+    
+
+
+    const renderOptions = () => {
+
+
+        const option = props.options.map((item, i) => {
+
+            const {name, property} = item;
+
+            const renderProperty = (property) => {
+
+                const propertys = property.map((elem, i) => {
+                   
+
+                    if(i === 0) {
+                        
+                        return (
+                            <li key={i} className="active">{elem}</li>
+                        )
+                    }
+
+                    return (
+                        <>
+                            <li key={i}>{elem}</li>
+                        </>
+                    )
+                })
+
+
+                return (
+                    <ul onClick={e => {console.log(e.target)}}>
+                       {propertys} 
                     </ul>
-                </span>
-                <span className="ticker__arrow" onClick={showProperty}>
-                    <svg width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1 1L9 8M16.3415 1L8.3415 8" stroke="white"/>
-                    </svg>
-                </span>
-            </div>
+                )
+            }
+            console.log(property)
+            const listProperty = renderProperty(property)
+            
+            return (
+                <div key={i} className="ticker__option ">
+                    <div 
+                    className="ticker__config">
+                        <span className="ticker__name-property">
+                            {name}:  
+                        </span>
+                        <span className="ticker__property">
+                            <ul>
+                                <li className='change'>{property[0]}</li>
+                                <li 
+                                className="list-property"
+                                ref={e => myRefs.current[i] = e}>
+                                    {listProperty}
+                                    <div className="ticker__property-change"></div>
+                                </li>
+                            </ul>
+                        </span>
+                        <span className="ticker__arrow" 
+                        
+                        onClick={() => showProperty(i)}>
+                            <svg width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 1L9 8M16.3415 1L8.3415 8" stroke="white"/>
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+            )
+        })
+
+        
+
+        return (
+            <>
+                {option}
+            </>
+        )
+    }
+
+    const items = renderOptions()
+
+    return (
+        <div className="ticker__options">
+            {items}
         </div>
     )
 }
