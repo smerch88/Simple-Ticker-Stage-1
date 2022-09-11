d1006917607e01c08d1c339a5672d547fee03faf
smerch88 <maxarmyk@gmail.com>
2022-09-11 16:17:13 +0300

    allowed hosts added again

--- Python_Code/taskmanager/taskmanager/settings.py
+++ Python_Code/taskmanager/taskmanager/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '0.0.0.0', 'simpleticker.online', 'www.simpleticker.online']
 
 
 # Application definition
