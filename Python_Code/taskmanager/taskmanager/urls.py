5e33a5c88fafa4b1091f8ae4fdc098f7e2ef4ca1
smerch88 <maxarmyk@gmail.com>
2022-09-11 17:44:59 +0300

    2 extra back pathes added for test

--- Python_Code/taskmanager/taskmanager/urls.py
+++ Python_Code/taskmanager/taskmanager/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('backend/admin/', admin.site.urls),
     path('backend/', include('main.urls')),
-    path('auth/', include('djoser.urls')),
+    path('backend/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken'))  # /token/login/ to get token, /logout/
 ]
