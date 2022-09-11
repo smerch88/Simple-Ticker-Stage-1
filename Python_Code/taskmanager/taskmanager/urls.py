7abb2197e09c4a2e693483a568479e140afb3721
smerch88 <maxarmyk@gmail.com>
2022-09-11 15:49:28 +0300

    backend path added

--- Python_Code/taskmanager/taskmanager/urls.py
+++ Python_Code/taskmanager/taskmanager/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('backend', include('main.urls')),
     path('auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken'))  # /token/login/ to get token, /logout/
 ]
