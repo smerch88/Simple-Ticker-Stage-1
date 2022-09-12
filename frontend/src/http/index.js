b5afef31375c3518a4c2297757fb329f3719320e
Ruslan Russel <selezen.ua30@gmail.com>
2022-09-12 10:52:48 +0300

    auth

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL === 'production' 
+  ? 'https://www.simpleticker.online' 
+  : 'http://127.0.0.1:8000';
 
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: API_URL
 })
 
 const $authHost = axios.create({
