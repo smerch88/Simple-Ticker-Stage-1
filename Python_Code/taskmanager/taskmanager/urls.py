a3ff47d955448a74401be879113901ae57e55aa7
HoderTon <xoderton@gmail.com>
2022-09-12 10:37:07 +0300

    removed backend

--- Python_Code/taskmanager/taskmanager/urls.py
+++ Python_Code/taskmanager/taskmanager/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('backend/admin/', admin.site.urls),
-    path('backend/', include('main.urls')),
-    path('backend/auth/', include('djoser.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken'))  # /token/login/ to get token, /logout/
 ]
