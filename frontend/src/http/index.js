c1e906223cd9407a536b790203cb5e2f0e54e22f
Alexander Osnova <kephik2000@gmail.com>
2022-09-12 23:34:13 +0300

    fix axios config

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'http://www.simpleticker.online' 
+  ? 'https://www.simpleticker.online' 
   : 'http://127.0.0.1:8000';
 
 const $host = axios.create({
