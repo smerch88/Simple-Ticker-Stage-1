a27a3394d0804d9452e012db55e576b57548e7cb
Ruslan Russel <selezen.ua30@gmail.com>
2022-09-15 23:22:12 +0300

    test autorization

--- frontend/src/http/userAPI.js
+++ frontend/src/http/userAPI.js
@@ -2,11 +2,11 @@ import { $authHost, $host } from ".";
 
 export const registration = async ({username, password, email}) => {
     const response = await $host.post('/auth/users/', {username, email, password})
-    return response
+    console.log(response)
 }
 export const login = async ({username,  password}) => {
     const response = await $host.post('/auth/token/login/', {username,  password})
-    return response
+    console.log(response)
 }
 export const chek = async () => {
     const response = await $host.post('/auth/users/')
