be0a8d68b2744a21a728bec7e84083dc3ed7e692
Ruslan Russel <selezen.ua30@gmail.com>
2022-09-15 22:47:39 +0300

    Revert "Update index.js"
    This reverts commit b8aa527bc53da8334ea018cb69e7c1ed9166c855.

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL === 'production' 
   ? 'https://www.simpleticker.online' 
-  : 'https://127.0.0.1:8000';
+  : 'http://127.0.0.1:8000';
 
 const $host = axios.create({
     baseURL: API_URL
