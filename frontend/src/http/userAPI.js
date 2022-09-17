03f2214e8e748871d996d0c482a6054119ca11f7
Ruslan Russel <selezen.ua30@gmail.com>
2022-09-17 14:29:09 +0300

    post crypto

--- frontend/src/http/userAPI.js
+++ frontend/src/http/userAPI.js
@@ -1,5 +1,6 @@
 import { $authHost, $host } from ".";
 import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 export const registration = async ({username, password, email}) => {
     const response = await $host.post('/auth/users/', {username, email, password})
@@ -17,14 +18,12 @@ export const chek = async () => {
 }
 
 export const logout = async () => {
-    const response = await $authHost.post('/auth/token/logout/')
-    
-    console.log(response)
+    localStorage.removeItem('token')
 }
 
 export const crypto = async (data) => {
     const headers = `Authorization: Token ${localStorage.getItem('token')}`
     const token = localStorage.getItem('token')
-    const response = await $host.post('/api/update_ticker_setup/', data)
+    const response = await axios.post('/api/update_ticker_setup/', data)
     console.log(response)
 }
