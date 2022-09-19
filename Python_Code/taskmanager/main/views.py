05ff88b0ee16480334d87b08c1d4eed691e17a02
smerch88 <maxarmyk@gmail.com>
2022-09-19 12:01:45 +0300

    update_ticker_setup modified

--- Python_Code/taskmanager/main/views.py
+++ Python_Code/taskmanager/main/views.py
@@ -107,9 +107,9 @@ def update_ticker_setup(request):
     global TICKER_SETUP
 
     if request.method == 'POST':
-        TICKER_SETUP = json.loads(str(request.POST.get('crypto_section')))
+        TICKER_SETUP = json.loads(request.body.decode())['crypto_section']
         return HttpResponse(status=200)
-    return HttpResponse(request.POST.get('crypto_section'))
+    return HttpResponse('Request method should be POST')
 
 @csrf_exempt
 def info_to_device(request):
